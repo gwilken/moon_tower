d43a0877ab77513b73cb4bbd5c7fe055b7a0f89f
g wilken <greg@gwilken.com>
2019-01-26 10:32:33 -0800

    gps.py changes

--- server_scripts/gps_listener.py
+++ server_scripts/gps_listener.py
@@ -3,7 +3,12 @@
 from redisutils import *
 import json
 import gps
- 
+
+with open('moon_config.json', 'r') as f:
+    config = json.load(f)
+
+GPS_UPDATE = config['gps']['polling_freq']
+
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
@@ -15,10 +20,9 @@ def unpack_gps_data():
         if report['class'] == 'TPV':
             print(report)
             return report
-    except:
-        return
+    
 
 while True:
     unpack_gps_data()
-   # add_hash_update_set( 'gps', unpack_gps_data() )
+    #add_hash_update_set( 'gps', unpack_gps_data() )
     time.sleep(GPS_UPDATE)
