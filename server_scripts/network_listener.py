f185ccfba6cd03e502ab6e02458b2038fad8ba2f
g wilken <greg@gwilken.com>
2019-01-26 14:27:33 -0800

    network.py

--- server_scripts/network_listener.py
+++ server_scripts/network_listener.py
@@ -1,6 +1,14 @@
 from redisutils import *
 import socket
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 def is_connected():
   try:
     host = socket.gethostbyname('www.gwilken.com')
@@ -11,6 +19,10 @@ def is_connected():
   return False
 
 while True:
-  status = { "connected": is_connected() }
+  status = { 
+    "connected": is_connected(),
+    "wwanIp": get_ip_address('wwan0') 
+    }
+  
   add_system_hash('network', status)
   time.sleep(2)
