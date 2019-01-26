7c33bed15a2112357ec0a79a0b92479acbd9f446
g wilken <greg@gwilken.com>
2019-01-26 09:47:59 -0800

    gps.py changes

--- server_scripts/gps_listener.py
+++ server_scripts/gps_listener.py
@@ -14,22 +14,9 @@ def unpack_gps_data():
     try:        
         for new_data in gps_socket:
             if new_data:
-                data_stream.unpack(new_data)
-
-                print data_stream.TPV
-
                 try:
-                    gps_data = {
-                        'latitude': data_stream.TPV['lat'],
-                        'longitude': data_stream.TPV['lon'],
-                        'mode': data_stream.TPV['mode'],
-                        'time': data_stream.TPV['time'],
-                        'alt': data_stream.TPV['alt'],
-                        'track': data_stream.TPV['track'],
-                        'speed': data_stream.TPV['speed']  
-                    }
-
-                    return gps_data
+                    data_stream.unpack(new_data)
+                    return data_stream.TPV
 
                 except Exception as e:
                     print 'Error setting redis gps keys:', e
