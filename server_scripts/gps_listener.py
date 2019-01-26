896085996ebea324eddbe8232eed46ed3323fcc8
g wilken <greg@gwilken.com>
2019-01-26 10:38:38 -0800

    gps.py changes

--- server_scripts/gps_listener.py
+++ server_scripts/gps_listener.py
@@ -16,14 +16,15 @@ session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 def unpack_gps_data():
     try:
         report = session.next()
-        print(report)
-        return report
  
+        if report['class'] == 'TPV':
+            return report
+    
     except:
         return
     
 
 while True:
-    unpack_gps_data()
-    #add_hash_update_set( 'gps', unpack_gps_data() )
-    time.sleep(1)
+    #unpack_gps_data()
+    add_hash_update_set( 'gps', unpack_gps_data() )
+    time.sleep(GPS_UPDATE)
