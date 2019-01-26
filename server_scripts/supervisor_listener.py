099690eba10952e65421eeb877ad7d6859c0fe56
g wilken <greg@gwilken.com>
2019-01-25 19:21:36 -0800

    Updating redisutils, supervisor listener

--- server_scripts/supervisor_listener.py
+++ server_scripts/supervisor_listener.py
@@ -7,13 +7,8 @@ status = { "proceses": server.supervisor.getAllProcessInfo() }
 
 while True:
   # add_hash_update_set('supervisor', status)
-
-  try:
-    for key, value in status.items():
-      r.hset('supervisor-hash', key, value)
-      r.expire('supervisor-hash', 86400)
-
-  except Exception as e:
-      print('Error setting Redis keys:', e)
+  for key, value in status.items():
+    r.hset('supervisor-hash', key, value)
+    r.expire('supervisor-hash', 86400)
 
   time.sleep(10)
