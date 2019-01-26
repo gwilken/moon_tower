f68bf0bba5eb8bef0f019b358f6b3db7f8ae5288
g wilken <greg@gwilken.com>
2019-01-26 13:57:40 -0800

    Network listener

---
+++ server_scripts/network_listener.py
@@ -0,0 +1,15 @@
+import socket
+
+def is_connected():
+  try:
+    host = socket.gethostbyname('www.gwilken.com')
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+     pass
+  return False
+
+while True:
+  status = { "connected": is_connected() }
+  add_system_hash('network', status)
+  time.sleep(2)
