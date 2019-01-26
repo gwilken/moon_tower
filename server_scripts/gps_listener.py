d9ff3a97c2ed44e1d7d1ebec6649f6c2cd797edb
g wilken <greg@gwilken.com>
2019-01-26 10:07:30 -0800

    gps.py changes

--- server_scripts/gps_listener.py
+++ server_scripts/gps_listener.py
@@ -9,26 +9,26 @@ with open('moon_config.json', 'r') as f:
 
 GPS_UPDATE = config['gps']['polling_freq']
 
-def unpack_gps_data():
-    try:        
-        for new_data in gps_socket:
-            try:
-                data_stream.unpack(new_data)
-                return data_stream.TPV
-
-            except Exception as e:
-                print 'Error setting redis gps keys:', e
-            
-    except Exception as e:
-        print 'Error unpacking gps stream:', e
-
-    return None
-
 gps_socket = gps3.GPSDSocket()
 data_stream = gps3.DataStream()
 
 gps_socket.connect()
 gps_socket.watch()
+
+def unpack_gps_data():
+    try:        
+        for new_data in gps_socket:
+            if new_data:
+                try:
+                    data_stream.unpack(new_data)
+                    return data_stream.TPV
+
+                except Exception as e:
+                    print 'Error setting redis gps keys:', e
+            
+    except Exception as e:
+        print 'Error unpacking gps stream:', e
+
 
 while True:
     add_hash_update_set( 'gps', unpack_gps_data() )
