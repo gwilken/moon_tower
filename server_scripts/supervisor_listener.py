73d0656f8c202ecdf89b52091a51884e2c8b0ee7
g wilken <greg@gwilken.com>
2019-01-25 19:15:09 -0800

    Updating redisutils, supervisor listener

--- server_scripts/supervisor_listener.py
+++ server_scripts/supervisor_listener.py
@@ -3,7 +3,7 @@ import json
 import xmlrpclib
 
 server = xmlrpclib.Server('http://192.168.2.3:9001/RPC2')
-status = { "proceses": server.supervisor.getAllProcessInfo() }
+status = server.supervisor.getAllProcessInfo()
 
 while True:
   # add_hash_update_set('supervisor', status)
