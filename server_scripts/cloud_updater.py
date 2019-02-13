c9de9aef3482f47880b53468fc06b40668f9cf61
greg <greg@gwilken.com>
2019-02-12 23:29:02 -0800

    Test routes

--- server_scripts/cloud_updater.py
+++ server_scripts/cloud_updater.py
@@ -4,16 +4,35 @@ import requests
 import json
 import time
 
-#r = requests.get('https://api.github.com/events')  
-#clo  print(r.content)
 
 mooncontrol = CustomCloud(None, send_host='www.gwilken.com', send_port=4000, network='cellular')
 
+print “version and type=”, mooncontrol.version, mooncontrol.network_type
+
+result1 = mooncontrol.network.connect()
+
+print “result1=”,result1
+
+print 'first CONNECTION STATUS: ’ + str(mooncontrol.network.getConnectionStatus())
+
+resp = mooncontrol.sendMessage(“abc”)
+
+print “resp=”,resp
+print 'second CONNECTION STATUS: ’ + str(mooncontrol.network.getConnectionStatus())
+
+
+
+#r = requests.get('https://api.github.com/events')  
+#clo  print(r.content)
+
+
+
 lastgps = get_last_hash('gps-set')
 
-res = mooncontrol.sendMessage("TEST TEST TEST", timeout = 30)
-print(res)
+#res = mooncontrol.sendMessage("TEST TEST TEST", timeout = 30)
 
-time.sleep(30)
+# print(res)
+
+#time.sleep(30)
 
 #r = requests.post('http://gwilken.com:4000/api/key/', json = lastgps)
