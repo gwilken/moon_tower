b01e1b65989522b28ee9eda7b69848d9a6211d44
greg <greg@gwilken.com>
2019-02-12 23:45:58 -0800

    Test routes

--- server_scripts/cloud_updater.py
+++ server_scripts/cloud_updater.py
@@ -9,7 +9,7 @@ mooncontrol = CustomCloud(None, send_host='www.gwilken.com', send_port=4000, net
 
 print mooncontrol.version, mooncontrol.network_type
 
-modem = mooncontrol.network.ms2131(None, 9600, '/etc/ppp/chatscripts/ms2131')
+modem = mooncontrol.network.MS2131(None, 9600, '/etc/ppp/chatscripts/ms2131')
  
 print modem
 
