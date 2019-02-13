1fa73657f64fc41ad2d41f8e88cad5a8f75936e1
greg <greg@gwilken.com>
2019-02-12 22:45:51 -0800

    Test routes

--- server_scripts/cloud_updater.py
+++ server_scripts/cloud_updater.py
@@ -1,10 +1,14 @@
-from Hologram.HologramCloud import HologramCloud
+from Hologram.CustomCloud import CustomCloud
 from redisutils import *
 import requests
 
 #r = requests.get('https://api.github.com/events')  
 #clo  print(r.content)
 
+mooncontrol = CustomCloud(dict(), send_host='gwilken.com', send_port=4000)
+
 lastgps = get_last_hash('gps-set')
 
-r = requests.post('http://gwilken.com:4000/api/key/', json = lastgps)
+mooncontrol.sendMessage(lastgps, timeout = 10)
+
+#r = requests.post('http://gwilken.com:4000/api/key/', json = lastgps)
