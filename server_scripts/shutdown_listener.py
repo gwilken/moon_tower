a328ba5a56aa53714a16dd7cffa4f8655c037238
greg <greg@gwilken.com>
2019-03-27 19:32:03 -0700

    Added shutdown_listener

--- server_scripts/shutdown_listener.py
+++ server_scripts/shutdown_listener.py
@@ -2,10 +2,18 @@
  
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
 def shutdown(channel):
-    if GPIO.input(12) == GPIO.HIGH:
-        print('SHUTTING DOWN')
+    #if GPIO.input(12) == GPIO.HIGH:
+    start = time.time()
+    elapsed = 0
+    
+    while GPIO.input(12) == GPIO.HIGH:
+      elapsed = time.time() - start
+      print('ELAPSED:', elapsed)
+      time.sleep(1)
+      #print('SHUTTING DOWN')
  
 try:
     GPIO.setmode(GPIO.BCM)
