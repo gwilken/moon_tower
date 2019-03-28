14c6f91e6fb2885f7d1b333c3897675350632255
greg <greg@gwilken.com>
2019-03-27 19:41:28 -0700

    Added shutdown_listener

--- server_scripts/shutdown_listener.py
+++ server_scripts/shutdown_listener.py
@@ -24,8 +24,7 @@ try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(12, GPIO.RISING, callback=shutdown, bouncetime=200)
- 
-    message = raw_input('\nPress any key to exit.\n')
+   #  message = raw_input('\nPress any key to exit.\n')
  
 finally:
     GPIO.cleanup()
