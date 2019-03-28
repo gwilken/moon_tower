1be71e5e8720cdaf9ed8c355374992dd985ceeb0
greg <greg@gwilken.com>
2019-03-27 19:04:02 -0700

    Added shutdown_listener

--- server_scripts/shutdown_listener.py
+++ server_scripts/shutdown_listener.py
@@ -1,13 +1,22 @@
-from gpiozero import Button
-from signal import pause
-
-button = Button(12)
-
-def hello():
-  print("Button pressed!")
-
-#button.when_pressed = hello()
-
-print(button)
-
-pause()
+# coding=utf-8
+ 
+import RPi.GPIO as GPIO
+import datetime
+ 
+def my_callback(channel):
+    if GPIO.input(12) == GPIO.HIGH:
+        print('\n▼  at ' + str(datetime.datetime.now()))
+    else:
+        print('\n ▲ at ' + str(datetime.datetime.now()))
+ 
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(12, GPIO.BOTH, callback=my_callback)
+ 
+    message = raw_input('\nPress any key to exit.\n')
+ 
+finally:
+    GPIO.cleanup()
+ 
+print("Goodbye!")
