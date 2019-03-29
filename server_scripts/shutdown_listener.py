eebe939530c430552edea548fb88f48a72dbf834
greg <greg@gwilken.com>
2019-03-28 20:31:19 -0700

    Testing shutdown_listener

--- server_scripts/shutdown_listener.py
+++ server_scripts/shutdown_listener.py
@@ -2,23 +2,23 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 
-def shutdown(this):
+def shutdown(pin):
     start = time.time()
     elapsed = 0
     
-    while GPIO.input(12) == GPIO.HIGH:
+    while GPIO.input(12) == GPIO.LOW:
       elapsed = time.time() - start
       print('ELAPSED:', elapsed)
       
       if elapsed > 5:
-        subprocess.run(['echo', 'howdy! Shutting down...'])
+        subprocess.run(['sudo', 'shutdown now'])
       
       time.sleep(1)
  
 try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(12, GPIO.RISING, callback=shutdown, bouncetime=200)
+    GPIO.setup(12, GPIO.IN)
+    GPIO.add_event_detect(12, GPIO.FALLING, callback=shutdown, bouncetime=200)
     
     while True:
       time.sleep(1)
