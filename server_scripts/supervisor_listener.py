1ae4f88f4d809b7a9039faa4067f9d987d3e2934
greg <greg@gwilken.com>
2019-03-30 10:27:25 -0700

    debuggin

--- server_scripts/supervisor_listener.py
+++ server_scripts/supervisor_listener.py
@@ -9,4 +9,4 @@ server = xmlrpclib.Server('http://192.168.2.3:9001/RPC2')
 while True:
   status = { "proceses": json.dumps(server.supervisor.getAllProcessInfo() )}
   set_hash('supervisor', status, 10)
-  time.sleep(2)
+  time.sleep(10)
